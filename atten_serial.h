a24ea7e719fdc68929cfe031f124519b2ebb3b82
ilovegaoyuan <34481898+ilovegaoyuan@users.noreply.github.com>
2019-05-16 15:58:40 +0800

    pe4302serial

---
+++ atten_serial.h
@@ -0,0 +1,16 @@
+/*
+ * atten_serial.h
+ *
+ *  Created on: 2019-5-16
+ *      Author: moc
+ */
+
+#ifndef ATTEN_SERIAL_H_
+#define ATTEN_SERIAL_H_
+extern void WriteTope4302();
+
+
+
+
+
+#endif /* ATTEN_SERIAL_H_ */
